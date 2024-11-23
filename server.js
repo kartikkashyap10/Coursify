9e2ef7661a6726a8a1d05c7ce8f3ba60277392f2
kartik.kashyap <kartik.kashyap@lendenclub.com>
2024-11-23 12:15:15 +0530

    ugly way of routing

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
+createUserRoutes = require("./user.js");
+
+createUserRoutes(app);
+
+app.post("/course/purchase", () => {
+    res.json({
+        message: "Signin endpoint"
+    });
+});
+
+app.get("/course/preview", () => {
+    res.json({
+        message: "Signin endpoint"
+    });
 });
 
 app.listen(port, () => {
     console.log(`server up on port ${port}`);
-});
+}); 
